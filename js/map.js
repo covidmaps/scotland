19cea52317842481e944db933c7f769c54aaee98
markjswan <markswan98@gmail.com>
2020-05-09 19:19:44 +0100

    update on mouseover

--- js/map.js
+++ js/map.js
@@ -132,7 +132,7 @@ function draw(boundaries) {
         .attr("id", function(d) {return d.id})
         .attr("properties_table", function(d) { return create_table(d.properties)})
         .attr("d", path)
-        .on("click", function(d){ return select(d)});
+        .on("mouseover", function(d){ return select(d)});
 
     // add a boundary between areas
     g.append("path")
