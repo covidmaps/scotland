a9e89145e1f815fab66373656a5fda6eed0560b4
Calum McM <calummcmeekin@me.com>
2020-05-12 17:40:57 +0100

    Added bar graphs

--- js/graph.js
+++ js/graph.js
@@ -36,7 +36,8 @@ function graph_init(res, id){
         if (res == 'lad'){
             document.getElementById("ladDoc").style.display = "block";
             multi_line_graph("/g_mobility/"+id+".csv", "#graphGMS");
-            singe_line_graph("/lad/total_covid_deaths/"+ "S12000005"+ ".csv", "#covidDTGraph", "Total Covid Deaths");
+            singe_line_graph("/lad/total_covid_deaths/"+ id+ ".csv", "#covidDTGraph", "Total Covid Deaths");
+            bar_graph("/lad/loc_deaths/"+id+".csv", "#barGraph");
         }
         else{
 
@@ -329,6 +330,66 @@ function multi_line_graph(file, graphID){
             // run the updateChart function with this selected option
             update(dict1[selectedOption])
         })
+
+    })
+}
+
+function bar_graph(file, graphID){
+    // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 90, left: 40},
+        width = 460 - margin.left - margin.right,
+        height = 450 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select(graphID)
+      .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+
+    // Parse the Data
+    d3.csv("https://raw.githubusercontent.com/markjswan/covid-maps/master/data"+file, function(data) {
+
+    // X axis
+    var x = d3.scaleBand()
+      .range([ 0, width ])
+      .domain(data.map(function(d) { return d.Country; }))
+      .padding(0.2);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, 13000])
+      .range([ height, 0]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+
+    // Bars
+    svg.selectAll("mybar")
+      .data(data)
+      .enter()
+      .append("rect")
+        .attr("x", function(d) { return x(d.Country); })
+        .attr("width", x.bandwidth())
+        .attr("fill", "#69b3a2")
+        // no bar at the beginning thus:
+        .attr("height", function(d) { return height - y(0); }) // always equal to 0
+        .attr("y", function(d) { return y(0); })
+
+    // Animation
+    svg.selectAll("rect")
+      .transition()
+      .duration(800)
+      .attr("y", function(d) { return y(d.Value); })
+      .attr("height", function(d) { return height - y(d.Value); })
+      .delay(function(d,i){console.log(i) ; return(i*100)})
 
     })
 }
