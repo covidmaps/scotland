0f231acfec19a177d8069e7c2226e288901227cd
markjswan <markswan98@gmail.com>
2020-05-24 14:38:00 +0100

    update data table

--- get_lad_all_stats_table.py
+++ get_lad_all_stats_table.py
@@ -16,6 +16,38 @@ def get_week_num():
     return datetime.date.today().isocalendar()[1]
 
 
+def get_death_locations_df(path, week):
+
+    # Try get get the most recent data
+    try:
+        url = path + "covid-deaths-data-week-%d.zip" % week
+        conn = urllib.request.urlopen(url)
+        zipfile = ZipFile(BytesIO(conn.read()))
+
+        df_location = pd.read_csv(zipfile.open('covid-deaths-data-week-%d_Table 3 - deaths by location.csv' % week), header=3, encoding='unicode-escape')
+        idx_start = np.where(df_location['Unnamed: 0'] == 'Aberdeen City')[0][0]
+        idx_end = idx_start + 32
+        df_location_trimmed = df_location.iloc[idx_start:idx_end, :-2]
+
+
+        return df_location_trimmed
+
+    # If error, then get last week response
+    except urllib.error.HTTPError as e:
+        url = path + "covid-deaths-data-week-%d.zip" % week-1
+        conn = urllib.request.urlopen(url)
+        zipfile = ZipFile(BytesIO(conn.read()))
+
+
+        df_location = pd.read_csv(zipfile.open('covid-deaths-data-week-%d_Table 3 - deaths by location.csv' % week), header=3, encoding='unicode-escape')
+
+        idx_start = np.where(df_location['Unnamed: 0'] == 'Aberdeen City')[0][0]
+        print(idx_start)
+        idx_end = idx_start + 32
+        df_location_trimmed = df_location.iloc[idx_start:idx_end, :-2]
+        return df_location_trimmed
+
+
 def create_all_data_df(path):
 
     dict_id = {"S12000033":"Aberdeen City","S12000034":"Aberdeenshire","S12000041":"Angus","S12000035":"Argyll and Bute","S12000036":"City of Edinburgh","S12000005":"Clackmannanshire","S12000006":"Dumfries and Galloway","S12000042":"Dundee City","S12000008":"East Ayrshire","S12000045":"East Dunbartonshire","S12000010":"East Lothian","S12000011":"East Renfrewshire","S12000014":"Falkirk","S12000015":"Fife","S12000046":"Glasgow City","S12000017":"Highland","S12000018":"Inverclyde","S12000019":"Midlothian","S12000020":"Moray","S12000013":"Na h-Eileanan Siar","S12000021":"North Ayrshire","S12000044":"North Lanarkshire","S12000023":"Orkney Islands","S12000024":"Perth and Kinross","S12000038":"Renfrewshire","S12000026":"Scottish Borders","S12000027":"Shetland Islands","S12000028":"South Ayrshire","S12000029":"South Lanarkshire","S12000030":"Stirling","S12000039":"West Dunbartonshire","S12000040":"West Lothian"}
@@ -57,10 +89,14 @@ def convert_all_types(df_all):
 
 def wrapper():
 
+    week = get_week_num()
+    df_death_locations_trimmed = get_death_locations_df(path, 20)
+
+
     df_all = create_all_data_df(path)
     df_all = convert_all_types(df_all)
 
 
     headings = ['lad', 'covid_deaths_carehome', 'covid_deaths_non-institution', 'covid_deaths_hospital', 'covid_deaths_other', 'covid_deaths_total', 'all_deaths_carehome', 'all_deaths_non-institution', 'all_deaths_hospital', 'all_deaths_other', 'all_deaths_total']
     df_all[headings].replace({',':''}, regex=True)
-    df_all.to_json('table_data_updated.json')
+    df_all.to_json('data/lad/table_data.json')
