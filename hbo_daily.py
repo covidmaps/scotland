2eeda800dafb8e3dfc3e9e43d5bd5031a0d15d18
Calum McM <calummcmeekin@me.com>
2020-06-04 11:36:06 +0100

    [ADDED] NAT data gathering

--- hbo_daily.py
+++ hbo_daily.py
@@ -36,11 +36,12 @@ def convert_date_to_day(date):
     return day_of_year
 
 
-def get_hbo_data_by_date(health_board, data_type):
+def get_hbo_data_by_date(health_board, data_type, samples=-31):
     df = pd.read_csv(path_dict.get(data_type))
     df = df[['Date', health_board]]
     df = df.rename(columns={'Date':'date', health_board:'value'})
-    df = df.iloc[-31:]
+    if (samples == -31):
+        return df.iloc[samples:]
     return df
 
 
@@ -61,30 +62,72 @@ def get_all_hbo_data(path_dict, root_path):
 
 def get_total_hbo_cases(path_dict, root_path):
     totals = {}
+    totalArray = {}
 
-    totalArray = []
+    newCasesTotal = []
+    icuTotals = []
 
+    # Get total COVID-19 cases
     for i in range (1, len(hbo_dict)+1):
-        df = get_hbo_data_by_date(hbo_dict[i], 'cases')
+
+        # Get total COVID-19 cumulative cases
+        df = get_hbo_data_by_date(hbo_dict[i], 'cases', 0)
+
+        # On first run through create the array that will store all totals
+        # for each day
+        if (i == 1):
+            print (len(df.index))
+            totalArray = np.zeros(len(df.index))
 
         # Convert value from object to int for summing
+        df['value'] = df['value'].replace({'\*': '0'}, regex=True)
         df['value'] = df['value'].astype(str).astype(int)
 
-        # Add value to totalCases array
-        totalArray.append(df['value'].tail(1).sum())
+        # Easiest to convert 'df' to numpy array for adding the value of each
+        # date with that of every health board and then saving the result as csv
+        numpyDF = df.to_numpy()
+        values = df['value'].to_numpy()
+        print ("VAL: " + str(values))
+        totalArray = np.add(totalArray,values)
+        print ("TOT BEFORE: " + str(totalArray))
+        print ("TOT: " + str(totalArray))
 
-    # Total number of cases
-    totalCasesSum = np.sum(totalArray)
+        # Get total COVID-19 cases today
+        lastTwo = df['value'].tail(2)
+        newCasesTotal.append(lastTwo.iloc[1] - lastTwo.iloc[0])
+
+        # Get total ICU beds occupied
+        df_icu = get_hbo_data_by_date(hbo_dict[i], 'icu')
+        latestNumber = df_icu['value'].tail(1)
+        latestNumber.astype(str)
+
+        # Filter out '*' terms
+        if (latestNumber.iloc[0] != '*'):
+            icuTotals.append(latestNumber.astype(int).sum())
+
+    # For every date replace its value with national total
+    for i in range(0, len(numpyDF)):
+        numpyDF[i][1] = totalArray[i]
+
+    totalCasesSum = totalArray[-1]
+    newCasesTotalSum = np.sum(newCasesTotal)
+    icuTotalsSum = np.sum(icuTotals)
+
     totals['cases'] = [totalCasesSum]
+    totals['daily'] = [newCasesTotalSum]
+    totals['icu'] = [icuTotalsSum]
 
-
-    df_totals = pd.DataFrame(totals, columns = ['cases'])#, 'hospital_covid', 'hospital_all', 'icu'])
+    # Convert numpy arrays to panda's dataframes
+    df_totals = pd.DataFrame(totals, columns = ['cases', 'daily', 'icu'])
+    df_daily_totals = pd.DataFrame(numpyDF, columns = ['date', 'value'])
 
     current_dir = os.getcwd()
 
-    df_totals.to_csv(current_dir + root_path + "totals.csv")
+    print (df_daily_totals.dtypes)
 
-    print (df_totals)
+    # Save dataframes
+    df_daily_totals.to_csv(current_dir + root_path + "daily_nat_totals.csv")
+    df_totals.to_json(current_dir + root_path + "totals.json")
 
 
 def get_all_hbo_data_dict(hbo_list, path_dict, root_path):
@@ -93,12 +136,11 @@ def get_all_hbo_data_dict(hbo_list, path_dict, root_path):
         for data_type in path_dict.keys():
 
             # Create the dataframe
-            print(hbo_dict[i])
             df = get_hbo_data_by_date(hbo_dict[i], data_type)
             path = root_path + f"{i}_{data_type}.csv"
             current_dir = os.getcwd()
             curr = os.path.dirname(os.getcwd())
-            #print(curr)
+
             df.to_csv(current_dir + path)
 
 
